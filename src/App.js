601e19303bd458ece915e216fc27fa23bca73e56
rykel262 <gabry.x.puglisi@gmail.com>
2022-04-19 00:14:09 +0200

    creazione customTheme

--- src/App.js
+++ src/App.js
@@ -4,6 +4,61 @@ import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ClippedDrawer from "./components/clippedDrawer/ClippedDrawer";
 
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        type: "light",
+        primary: {
+            main: "#8ecae6",
+        },
+        secondary: {
+            main: "#8ee6d6",
+        },
+        text: {
+            primary: "#000000",
+        },
+    },
+    typography: {
+        fontSize: 15,
+        fontWeightRegular: 400,
+        fontWeightBold: 600,
+        h1: {
+            fontFamily: "Nunito",
+        },
+        h2: {
+            fontFamily: "Nunito",
+        },
+        h3: {
+            fontFamily: "Nunito",
+        },
+        h4: {
+            fontFamily: "Nunito",
+        },
+        h5: {
+            fontFamily: "Nunito",
+        },
+        overline: {
+            fontFamily: "Nunito",
+        },
+        caption: {
+            fontFamily: "Nunito",
+        },
+        button: {
+            fontFamily: "Nunito",
+        },
+        body2: {
+            fontFamily: "Nunito",
+        },
+        subtitle1: {
+            fontFamily: "Nunito",
+        },
+        subtitle2: {
+            fontFamily: "Nunito",
+        },
+    },
+});
+
 function App() {
     return (
         <div className="App">
