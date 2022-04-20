25c37a12db9f4b7d12c7934be3511059dcc714c6
rykel262 <gabry.x.puglisi@gmail.com>
2022-04-20 20:01:18 +0200

    first checkpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ClippedDrawer from "./components/clippedDrawer/ClippedDrawer";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        type: "light",
+        primary: {
+            main: "#8ecae6",
+        },
+        secondary: {
+            main: "#8ee6d6",
+        },
+        text: {
+            primary: "#000000",
+        },
+    },
+    typography: {
+        fontFamily: "Nunito",
+        fontSize: 13,
+        fontWeightRegular: 400,
+        fontWeightBold: 600,
+    },
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/">
+                            <Route index element={<Home />} />
+                            <Route path="login" element={<Login />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
