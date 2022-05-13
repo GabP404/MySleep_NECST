80f0a5dff7b2f94096b557046f65437a8be16450
GabryMacbook <gabrielepuglisi@Gabrieles-Macbook-Pro.local>
2022-05-14 00:08:07 +0200

    aggiunta pagina upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import Upload from "./pages/upload/Upload";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ClippedDrawer from "./components/clippedDrawer/ClippedDrawer";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
@@ -33,8 +33,8 @@ function App() {
                 <BrowserRouter>
                     <Routes>
                         <Route path="/">
-                            <Route index element={<Home />} />
-                            <Route path="login" element={<Login />} />
+                            <Route path="home" element={<Home />} />
+                            <Route index element={<Upload />} />
                         </Route>
                     </Routes>
                 </BrowserRouter>
